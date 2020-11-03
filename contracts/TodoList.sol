9ee49a3868368341d9418ce01e3bbcc336fe4c04
Ivan Brygar <smitbmx@gmail.com>
2020-11-03 19:42:14 +0800

    Added the Task entity to the contract, mapping, constructor and the createTask method

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,22 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Check my portfolio ivanbrygar.net");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
