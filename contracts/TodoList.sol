53ce8199c67b41db86a59d84a05d642e8d000070
Ivan Brygar <smitbmx@gmail.com>
2020-11-09 18:33:11 +0800

    Added the TaskCreated event with subscription. Added 1 more test 'creates tasks'.

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,13 +11,16 @@ contract TodoList {
 
     mapping(uint256 => Task) public tasks;
 
+    event TaskCreated(uint256 id, string content, bool completed);
+
     constructor() public {
         createTask("Check my portfolio ivanbrygar.net");
     }
 
     function createTask(string memory _content) public {
         taskCount++;
-
         tasks[taskCount] = Task(taskCount, _content, false);
+
+        emit TaskCreated(taskCount, _content, false);
     }
 }
